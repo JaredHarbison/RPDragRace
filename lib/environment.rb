b08fd15333f66bde2ed6e07325aa7d57c4ae530a
Jared Harbison <jared.harbison@gmail.com>
2019-02-07 14:57:35 +0000

    updated file structure

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,1 +1,8 @@
-require "RPDragRace/version"
+require "pry"
+require "nokogiri"
+require "open-uri"
+
+require_relative '../lib/RPDragRace/scraper'
+require_relative '../lib/RPDragRace/cli'
+require_relative '../lib/RPDragRace/version'
+require_relative '../lib/RPDragRace/queen'
