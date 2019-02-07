169cca251342b78731ed437ee988d13109a27fea
Jared Harbison <jared.harbison@gmail.com>
2019-02-07 19:30:24 +0000

    progress

--- lib/environment.rb
+++ lib/environment.rb
@@ -7,6 +7,6 @@ module RPDragRace
 end 
 
 require_relative 'RPDragRace/scraper'
-require_relative 'RPDragRace/cli'
+require_relative 'RPDragRace/CLI'
 require_relative 'RPDragRace/version'
 require_relative 'lib/RPDragRace/queen'
