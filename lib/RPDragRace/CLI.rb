c42260adb8a68cb2d666652f958bda2051444962
Jared Harbison <jared.harbison@gmail.com>
2019-02-08 00:27:02 +0000

    progress

--- lib/RPDragRace/CLI.rb
+++ lib/RPDragRace/CLI.rb
@@ -1,11 +1,14 @@
 class RPDragRace::CLI 
   
-  def call
+def call
     RPDragRace::Scraper.new.make_queens
     puts "--------    The library is open!     --------"
     puts "- Welcome to Ru Paul's Drag Race info-rama! -"
     puts "------ It's time to make your decision ------"
+    puts ""
     list_queens 
+    puts ""
+    choose_your_queen
   end 
   
   def list_queens
@@ -15,6 +18,24 @@ class RPDragRace::CLI
     puts "Naomi Smalls"
     puts "Trinity Taylor"
   end
+  
+  def choose_your_queen
+    puts "---------     Choose your queen     ---------"
+    puts "------------    type her name    ------------"
+    input = gets.strip 
+    case input
+    when "Latrice Royale"
+      puts "She is large and in charge, chunky, yet funky. Bold and Beautiful baby."
+    when "Monet Exchange"
+      puts "Guess who's black in the house, bitch!"
+    when "Monique Heart"
+      puts "Honey, the face, the look, the meme. Miss Monique Heart. Reclaiming my time, reclaiming my crown, RuPaul where's my check at?"
+    when "Naomi Smalls"
+      puts "You can't spell legendary without...leg!"
+    when "Trinity Taylor"
+      puts "Yes! The body is back!"
+    end 
+  end 
   
   def start 
     puts "Would you like to see a list of queens? Type yes or no"
