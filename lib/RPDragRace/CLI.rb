ff1b2c32c7f9edca66fa208967898b084ac15b66
Jared Harbison <jared.harbison@gmail.com>
2019-02-08 02:12:59 +0000

    updates

--- lib/RPDragRace/CLI.rb
+++ lib/RPDragRace/CLI.rb
@@ -18,27 +18,33 @@ def call
   def choose_your_queen
     input = nil 
     while input != "exit"
-    puts "---------     Choose your queen     ---------"
-    puts "------    type her name or type exit   ------"
-    input = gets.strip
-    case input
-    when "Latrice Royale"
-      puts "She is large and in charge, chunky, yet funky. Bold and Beautiful baby."
-    when "Monet Exchange"
-      puts "Guess who's black in the house, bitch!"
-    when "Monique Heart"
-      puts "Honey, the face, the look, the meme. Miss Monique Heart. Reclaiming my time, reclaiming my crown, RuPaul where's my check at?"
-    when "Naomi Smalls"
-      puts "You can't spell legendary without...leg!"
-    when "Trinity Taylor"
-      puts "Yes! The body is back"
-    when "exit"
-      goodbye
-    else 
-      puts "Try again!"
+      puts "---------     Choose your queen     ---------"
+      puts "------    type her name or type exit   ------"
+      input = gets.strip
+    case input   
+      when "Latrice Royale"
+        puts "She is large and in charge, chunky, yet funky. Bold and Beautiful baby."
+      when "Monet Exchange"
+        puts "Guess who's black in the house, bitch!"
+      when "Monique Heart"
+        puts "Honey, the face, the look, the meme. Miss Monique Heart. Reclaiming my time, reclaiming my crown, RuPaul where's my check at?"
+      when "Naomi Smalls"
+        puts "You can't spell legendary without...leg!"
+      when "Trinity Taylor"
+        puts "Yes! The body is back"
+      when "exit"
+        goodbye
+      else 
+        puts "Try again!"
+      end 
     end 
   end 
-end 
+  
+  def your_queen(queen)
+    puts ""
+    puts "#{queen.name}"
+    puts "#{queen.quote}"
+  end 
   
   def goodbye
     puts "Now, sashay away!"
--- lib/RPDragRace/scraper.rb
+++ lib/RPDragRace/scraper.rb
@@ -8,7 +8,7 @@ class RPDragRace::Scraper
     self.get_page.css('.tabber').last.css('.thumbimage')
   end 
   
-######## filter out duplicates before iteration below ########
+######## filter out duplicates below before iteration (.each) ########
 
   def make_queens
     scrape_queens.each do |q|
