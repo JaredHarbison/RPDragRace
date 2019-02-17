0d8a6e2d4b7954e440baef05a903c180c4cbaa89
Jared Harbison <jared.harbison@gmail.com>
2019-02-17 19:18:24 +0000

    progress

--- lib/RPDragRace/queen.rb
+++ lib/RPDragRace/queen.rb
@@ -29,41 +29,11 @@ class RPDragRace::Queen
   end 
   
   def statistics
-    @statistics ||= doc.css("#mw-content-text > table.wikitable").text 
+    @statistics ||= doc.css("#mw-content-text > table.wikitable").text
   end 
   
   def quotes
-    @quotes ||= doc.css('#mw-content-text > ul > li').text
+    @quotes ||= doc.css('#mw-content-text > ul:nth-of-type(1) > li').text
   end 
 
 end 
-
-  
-  def tryout
-    #@tryout ||= 
-    #doc.css('.Memorable_Quotes').collect do |quote|
-      #quote.css('li').text
-    #end 
-    #doc.search('#mw-content-text > ul > li')
-    #doc.at_css("span").next_sibling().text 
-    #doc.css("#Memorable_Quotes", "ul li").text
-    #page.xpath('//li[@class="clearfix"]').map do |item|
-    #This says, hey Nokogiri, for the page object, return all list items with a class of ‘clearfix’ on them - and lets map and iterate over them.
-    #title = item.at_xpath('.//h2').text.strip
-  end 
- 
-
-  #def quotes
-    #@quotes ||= doc.css('#mw-content-text > ul > li').text
-    ##Entrance_Quotes
-    #page.css('div#reference a')
-    #page.css('div#entrance_quotes')[0]
-    #tagcloud_elements = nokogiri_object.xpath("//ul[@class='tagcloud']/li/a")
-      #tagcloud_elements.each do |tagcloud_element|
-        #puts tagcloud_element.text
-      #end
-    #ul.tagcloud  > li >  a
-    ##mw-content-text > ul:nth-child(24) > li:nth-child(1)
-    ##mw-content-text > ul:nth-child(24) > li:nth-child(1)
-    ##mw-content-text > ul
-  #end 
