9aece0e83a577f7bf01e03638c885cb99e499c45
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 02:40:13 +0000

    progress

--- lib/RPDragRace/cli.rb
+++ lib/RPDragRace/cli.rb
@@ -1,11 +1,11 @@
-class RPDragRace::CLI 
-  
+class RPDragRace::CLI
+
   def call
-    introduction 
+    introduction
     list_queens
-    make_your_selection 
-    goodbye 
-  end 
+    make_your_selection
+    goodbye
+  end
 
   def introduction
     puts ""
@@ -18,49 +18,49 @@ class RPDragRace::CLI
     puts "------------  Well #{@user_name}  ------------"
     puts "-----  It's time to make your decision  -----"
     puts ""
-  end 
-  
+  end
+
   def list_queens
     RPDragRace::Scraper.new.get_queens
-    @queens = RPDragRace::Queen.all[0..185]
-    @queens.each.with_index(1) do |queen, i, url|   
-      puts "#{i}. #{queen.name}" 
-    end 
-  end 
-  
+    @queens = RPDragRace::Queen.remove_dup[0..185]
+    @queens.each.with_index(1) do |queen, i, url|
+      puts "#{i}. #{queen.name}"
+    end
+  end
+
   def make_your_selection
     choose_your_queen
-    input = nil 
+    input = nil
     while input != "exit"
-      input = gets.strip 
-      if input.to_i > 0 
+      input = gets.strip
+      if input.to_i > 0
         choice = RPDragRace::Queen.find(input.to_i)
         read_queen(choice)
         chose_again
       elsif input == "list"
         list_queens
         choose_your_queen
-      else  
-      end 
+      else
+      end
     end
-  end 
+  end
 
   def choose_your_queen
     puts ""
     puts "------------  Choose your queen  ------------"
-    puts "------  type her number or type exit   ------"    
+    puts "------  type her number or type exit   ------"
     puts "----  To see the list again type 'list'. ----"
     puts ""
-  end 
-  
+  end
+
   def chose_again
     puts ""
     puts "----  Would you like to choose another?  ----"
     puts "- If so type her number. If no type 'exit'. -"
     puts "----  To see the list again type 'list'. ----"
     puts ""
-  end 
-  
+  end
+
   def read_queen(choice)
     puts ""
     puts "---------------------------------------------"
@@ -78,13 +78,13 @@ class RPDragRace::CLI
     puts "---------------------------------------------"
     puts ""
     puts "#{choice.trivia}"
-  end 
-      
+  end
+
   def goodbye
     puts ""
     puts "So it is written, and so it shall be."
     puts "Now #{@user_name}, sashay away!"
     puts ""
-  end 
-  
+  end
+
 end
