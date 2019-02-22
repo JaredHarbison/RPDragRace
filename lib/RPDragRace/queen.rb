df39b7299ffa75580a7aa812df50afaaeb333432
Jared Harbison <jared.harbison@gmail.com>
2019-02-22 01:39:26 +0000

    progress

--- lib/RPDragRace/queen.rb
+++ lib/RPDragRace/queen.rb
@@ -1,10 +1,14 @@
-class RPDragRace::Queen 
+class RPDragRace::Queen
   attr_accessor :name, :url, :statistics, :biography, :trivia
   @@all = []
 
   def self.queens_page(q)
-    self.new(q.attr("alt").split('_').join(' '), "https://rupaulsdragrace.fandom.com/wiki/" + q.attr("alt").split(' ').join('_'))
-  end 
+    I18n.enforce_available_locales = false
+    @url = I18n.transliterate("https://rupaulsdragrace.fandom.com/wiki/#{q.attr("alt")}").split(' ').join('_')
+    #binding.pry #if q.attr("alt")[0..2] == "M"https://rupaulsdragrace.fandom.com/wiki/#{q.attr("alt")}"on"
+    self.new(q.attr("alt").split('_').join(' '), url)
+    #self.new(q.attr("alt").split('_').join(' '), "https://rupaulsdragrace.fandom.com/wiki/" + q.attr("alt").split(' ').join('_'))
+  end
 
   def initialize(name=nil, url=nil)
     @name = name
@@ -12,28 +16,32 @@ class RPDragRace::Queen
     @@all << self
   end
 
+  def self.remove_dup
+    @@all = @@all.select.with_index{|_, i| i.odd?}
+  end
+
   def self.all
-    @@all.select.with_index{|_, i| i.odd?}
+    @@all
   end
 
   def self.find(id)
-    self.all[id-1]
+    self.remove_dup[id-1]
   end
-  
+
   def doc
     @doc ||= Nokogiri::HTML(open(self.url))
   end
-   
+
   def biography
     @biography ||= doc.css("#mw-content-text > aside > section:nth-child(3)").text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '')
-  end 
-  
+  end
+
   def statistics
     @statistics ||= doc.css("#mw-content-text > table.wikitable").text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '')
-  end 
-  
-  def trivia
-    @trivia ||= doc.css('#mw-content-text > ul > li').text
-  end 
+  end
 
-end 
+  def trivia
+    @trivia ||= doc.css('#mw-content-text > ul > li')[0..doc.css('#mw-content-text > ul > li').length-6].text
+  end
+
+end
