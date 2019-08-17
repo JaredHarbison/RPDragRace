bfc5bdb95372f9298b45e9cb8bbe1b5c47275298
Jared Harbison <jared.harbison@gmail.com>
2019-08-17 09:13:22 -0400

    use following:: and preceding:: for trivia and quotes

--- lib/RPDragRace/queen.rb
+++ lib/RPDragRace/queen.rb
@@ -72,12 +72,6 @@ class RPDragRace::Queen
                                 /text()').text
   end 
 
-  def ethnicity 
-    @ethnicity ||= doc.xpath('//*[@data-source="Ethnicity"]
-                            /div[@class="pi-data-value pi-font"]
-                            /text()').map { |e| e.text.split(' ').join(' ').gsub(/[^0-9a-z%&!\n\/(). ]/i, '') }
-  end
-
   def date_of_birth 
     @date_of_birth ||= doc.xpath('//*[@data-source="birth year"]
                                 /div[@class="pi-data-value pi-font"]
@@ -133,12 +127,6 @@ class RPDragRace::Queen
     @website = site ? site : official_site
   end 
 
-  def trivia ################## "trivia" top choice!! working perfectly just need to stop before Gallery
-    @trivia =  doc.xpath('//*[@id="Trivia"]/following::*/li')
-                         .map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
-                         #### clean and neat list, not an array
-  end
-
   def seasons 
     @seasons ||= doc.xpath('//*[@data-source="Season"]/div[@class="pi-data-value pi-font"]/a').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
   end 
@@ -159,89 +147,32 @@ class RPDragRace::Queen
     @challenge_wins ||= doc.xpath('//*[@data-source="Challenge Wins"]/child::text()').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
   end 
 
+  def ethnicity 
+    @ethnicity ||= doc.xpath('//*[@data-source="Ethnicity"]
+                            /div[@class="pi-data-value pi-font"]
+                            /text()').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
+  end
+
+  def quotes #### this one works great but the regex needs to get a little tighter
+    queen_quotes = doc.xpath('//*[preceding::*[@id="Quotes"] and following::*[@id="Trivia"]]').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') }
+    memorable_quotes =  doc.xpath('//*[preceding::*[@id="Memorable_Quotes"] and following::*[@id="Trivia"]]').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') }
+    #Xqueen_quotes = doc.xpath('//div[preceding-sibling::h2[1]="Quotes"]/text()')
+    #Xmemorable_quotes = doc.xpath('//div[preceding-sibling::h2[1]="Memorable Quotes"]/text()')
+    @quotes = queen_quotes + memorable_quotes
+  end 
+
+#  def quotes 
+#    queen_quotes = doc.xpath('//*[@id="Quotes"]/following::*/li').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
+#    memorable_quotes = doc.xpath('//*[@id="Memorable_Quotes"]/following::*/li').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
+#    @quotes = queen_quotes + memorable_quotes
+#  end 
+
+  def trivia 
+    @trivia = doc.xpath('//*[preceding::*[@id="Trivia"] and following::*[@id="Gallery"]]').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } 
+    #X@trivia = doc.xpath('//div[preceding-sibling::h2[1]="Trivia"]/text()')
+  end 
+
 end
 
 
-## doc.xpath('')
-## "trivia"
-################ https://docs.scrapy.org/en/xpath-tutorial/topics/xpath-tutorial.html
-################ doc.xpath('following::node()*[@id="Trivia"]')
 
-
-# !! doc.xpath('//*[@id="Trivia"]/following::*/li').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } #### clean and neat list, not an array
-# !! doc.xpath('//*[@id="Trivia"]/following::*/li/text()').text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') #### one of best so far - pulls all after Trivia span and a lot of whitespace
-# !! doc.xpath('//*[@id="Trivia"]/following::*/li') #### does pull lis, but doesn't stop at gallery
-# !! doc.xpath('//*[@id="mw-content-text"]/ul') ####pulls a LOT of ul/li, but not consistent
-# !! doc.xpath('//*[@id="mw-content-text"]/ul[2]/li[1]/text()') #### pulls exact, but not consistent
-# !! doc.xpath('//*[@id="Trivia"]/following::*/ul/li/text()') #### pulls only the first trivia
-# !! doc.xpath('//span[@id="Trivia"]') ####pulls same as //*[id...
-# !! doc.xpath('//*[@id="Trivia"]')
-# !! doc.xpath('//*[@id="Trivia"]').text ####pulls just "Trivia"
-# X doc.xpath('//*[@id="Trivia"]/ul')
-# X doc.xpath('//*[@id="Trivia"]/ul').text
-# X doc.xpath('//*[@id="Trivia"]//li')
-# X doc.xpath('//*[@id="Trivia"]//li').text
-# X doc.xpath('//*[@id="Trivia"]/ul/li')
-# X doc.xpath('//*[@id="Trivia"]/ul/li').text
-# X doc.xpath('//*[@id="Trivia"]::ul')
-# X doc.xpath('//*[@id="Trivia"]/following-sibling')
-# X doc.xpath('//*[@id="Trivia"]/following-sibling::ul')
-# X doc.xpath('//*[@id="Trivia"]/following-sibling::ul/li')
-# X doc.xpath('//following-sibling::*[@id="Trivia"]')
-# X doc.xpath('//following-sibling::[@id="Trivia"]')
-# X doc.xpath('following-sibling[ul/li]::span[@id="Trivia"]')
-# X doc.xpath('//*[@id="mw-content-text"]/ul/text()')
-# X doc.css('#mw-content-text > ul:nth-child(40)').text
-
-#### to try, for @ trivia
-#### - following <h2>Trivia</h2> | prececing <h2>Gallery</h2>
-
-#### removed, handling in the real_name method
-####  def first_name
-####    @real_name[0] || "Ryan"
-####  end
-
-####  def last_name
-####    @real_name[-1] || "Taylor"
-####  end
-  #### for trivia, try something modeled after this - /*/p[count(preceding-sibling::divider)=1]
-  #### for trivia, "//ul/li[preceding-sibling::li='doprep' and following-sibling::li='Savior']"
-  #def trivia_2 ################ "trivia_2"
-  #  @trivia_2 = doc.xpath('//*[prececing-sibling::*[@id="Gallery"]]').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } #### clean and neat list, not an array
-  #end
-
-  #def trivia_3 ################ "trivia_3"
-  #  @trivia_3 = doc.xpath('//*[prececing-sibling::*[@id="Gallery"] and following-sibling::*[@id="Trivia"]]').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } #### clean and neat list, not an array
-  #end
-
-  #def trivia_4 ################ "trivia_4" pulls the appropriate section header
-  #  @trivia_4 = doc.xpath('//*[@id="Trivia"]/following::*/li').map { |e| e.text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '') } #### clean and neat list, not an array
-  #end
-
-
-#  def age
-#    @age ||= doc.css("#mw-content-text > aside > section:nth-child(3) > div:nth-child(6) > div").text
-#  end
-#  def current_city
-#    @current_city ||= doc.css("#mw-content-text > aside > section:nth-child(3) > div:nth-child(8) > div").text
-#  end
-#  def season
-#    @season ||= doc.css("#mw-content-text > aside > section:nth-child(4) > div > a:nth-child(1)").text
-#  end
-#  def placement
-#    @placement ||= doc.css("#mw-content-text > aside > section:nth-child(4) > div:nth-child(3) > div").text
-#  end
-#  def eliminated
-#    @eliminated ||= doc.css("#mw-content-text > aside > section:nth-child(4) > div:nth-child(5) > div").text
-#  end
-#  def sent_home_by
-#  end
-#  def challenge_wins
-#  end
-#  def friends
-#  end
-#  def statistics
-#    @statistics ||= doc.css("#mw-content-text > table").text.gsub(/[^0-9a-z%&!\n\/(). ]/i, '')
-#  end
-#  #@ethnicity ||= doc.css("#mw-content-text > aside > section:nth-child(3) > div:nth-child(4) > div").text
-  
